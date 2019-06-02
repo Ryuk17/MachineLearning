47f77c357976f6abd9b381a44c21648ba043c399
Danc1elion <liujieit@foxmail.com>
2019-06-02 21:15:42 +0800

    Create Eclat_TEST.py

---
+++ examples/Eclat_TEST.py
@@ -0,0 +1,25 @@
+"""
+@ Filename:       Eclat_TEST.py
+@ Author:         Danc1elion
+@ Create Date:    2019-06-02   
+@ Update Date:    2019-06-02 
+@ Description:    Implement Eclat_TEST
+"""
+
+from AssociationAnalysis import Eclat
+import numpy as np
+import pandas as pd
+import time
+
+trainData = [['bread', 'milk', 'vegetable', 'fruit', 'eggs'],
+            ['noodle', 'beef', 'pork', 'water', 'socks', 'gloves', 'shoes', 'rice'],
+            ['socks', 'gloves'],
+            ['bread', 'milk', 'shoes', 'socks', 'eggs'],
+            ['socks', 'shoes', 'sweater', 'cap', 'milk', 'vegetable', 'gloves'],
+            ['eggs', 'bread', 'milk', 'fish', 'crab', 'shrimp', 'rice']]
+
+time_start1 = time.time()
+clf1 = Eclat()
+pred1 = clf1.train(trainData)
+time_end1 = time.time()
+print("Runtime of Eclat:", time_end1-time_start1)
