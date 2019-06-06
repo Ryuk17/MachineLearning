dcadcf2b34e06dab7e3f5cfbb81359378924ba39
Danc1elion <liujieit@foxmail.com>
2019-06-06 17:16:37 +0800

    Update PCA_TEST.py

--- examples/PCA_TEST.py
+++ examples/PCA_TEST.py
@@ -2,35 +2,41 @@
 @ Filename:       PCA_TEST.py
 @ Author:         Danc1elion
 @ Create Date:    2019-06-03   
-@ Update Date:    2019-06-03 
+@ Update Date:    2019-06-06 
 @ Description:    Implement PCA_TEST
 """
 
 from DimensionReduction import PCA
-import numpy as np
 from sklearn.decomposition import PCA as pca
+import numpy as np
 import time
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
 
-data = np.array([[2.5, 2.4],
-                [0.5, 0.7],
-                [2.2, 2.9],
-                [1.9, 2.2],
-                [3.1, 3.0],
-                [2.3, 2.7],
-                [2, 1.6],
-                [1, 1.1],
-                [1.5,1.6],
-                [1.1, 0.9]])
+trainData = np.array(pd.read_table('./dataset/dataset1/train.txt', header=None, encoding='gb2312', delim_whitespace=True))
+testData = np.array(pd.read_table('./dataset/dataset1/test.txt', header=None, encoding='gb2312', delim_whitespace=True))
+train_y = trainData[:, -1]
+train_x = np.delete(trainData, -1, axis=1)
+test_y = testData[:, -1]
+test_x = np.delete(testData, -1, axis=1)
+
 time_start1 = time.time()
 clf1 = PCA()
-clf1.train(data)
-print(clf1.transformData(data))
+clf1.train(train_x)
+train_x = clf1.transformData(train_x)
+test_x = clf1.transformData(test_x)
+clf = LogisticRegression(solver='liblinear', multi_class='ovr')
+clf.fit(train_x, train_y)
+print("Accuracy of PCA:", clf.score(test_x, test_y))
 time_end1 = time.time()
 print("Runtime of PCA:", time_end1-time_start1)
 
 time_start2 = time.time()
-clf1 = pca(1)
-x = clf1.fit_transform(data)
-print(x)
+clf2 = pca(n_components=1)
+train_x = clf2.fit_transform(train_x)
+test_x = clf2.fit_transform(test_x, test_y)
+clf = LogisticRegression(solver='liblinear' ,multi_class='ovr')
+clf.fit(train_x, train_y)
+print("Accuracy of sklearn PCA:", clf.score(test_x, test_y))
 time_end2 = time.time()
 print("Runtime of sklearn PCA:", time_end2-time_start2)
